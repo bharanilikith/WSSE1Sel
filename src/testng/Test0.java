1c927593c968e82f96069140519779ee7f4c7803
Bharanilikith <Bharanilikith@Bharani>
2022-09-02 16:24:39 +0530

    Testng

---
+++ src/testng/Test0.java
@@ -0,0 +1,14 @@
+package testng;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test0 {
+	
+	@Test
+	public void tc1() {
+		Reporter.log("Hello selenium",true);
+		Reporter.log("Hello java",false);
+	}
+
+}
